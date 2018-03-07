c4f138e91e68c82fc14e58f0a6120b70deeef184
AleksandarPetkov <A.PetkovPetkov@gmail.com>
2018-03-07 13:48:22 +0200

    First steps in coding added.

---
+++ 01. FirstStepsInCoding/04Triangle55Star/Triangle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Triangle
+{
+    static void Main()
+    {
+        for (int row = 1; row <= 10; row++)
+        {
+            Console.Write(new string('*', row));
+            Console.WriteLine(row);
+        }
+}
+
---
+++ 01. FirstStepsInCoding/04TriangleOf55Stars/Triangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Triangle
+{
+    static void Main()
+    {
+        for (int stars = 1; stars <=10 ; stars++)
+        {
+            Console.Write(new string('*',stars));
+            Console.WriteLine();
+                       
+        }
+    }
+}
+
